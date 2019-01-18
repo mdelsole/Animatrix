bd1eb19fd07d63b4bb6765912b7f1b1fa0376a7d
mdelsole <mdelsole64@gmail.com>
2019-01-18 16:29:10 -0500

    Worked on V1 further

--- vision/baseHierarchy/buildV1Filters.py
+++ vision/baseHierarchy/buildV1Filters.py
@@ -8,39 +8,62 @@ orientations (4 orientations per scale) while the outer loop runs through scales
 """
 
 
-def buildV1filters():
+def buildV1filters(orientations, rfsizes, div):
 
-    print("Building V1 filters")
-    # The finalized set of filters
-    filters = []
+    numOrientations = np.size(orientations)
+    numRfsizes = np.size(rfsizes)
 
-    # i is the receptive field size. We go from size of 7 to 30 in increments of 2
-    # This is a changeable parameter, but 7 to 30 is a good number
-    for i in range(7,30,2):
-        # Filters for this receptive field size
-        filtersThisSize = []
-        for o in range(0, 4):
-            # Four phases: 0, pi/4, pi/2, 3pi/4
-            theta = o * math.pi / 4
-            # Template grid for all the different angles in the phase
-            x, y = np.mgrid[0:i, 0:i] - i / 2
+    # Spatial aspect ratio
+    gamma = 0.3
+    # Array of the filter sizes
+    filterSizes = np.zeros((numRfsizes,1))
+    # Storage place for the finalized filters
+    # Column = biggest rfsize, squared because rfs are square. Row = for each different rfsize
+    filters = np.zeros((np.max(rfsizes)**2,numRfsizes))
+
+    for k in range(numRfsizes):
+        for o in range(numOrientations):
+            # Angle
+            theta = orientations[o]*math.pi/180
+            # Size of current filter
+            filterSize = rfsizes[k]
+            # Center of filter, i.e. midpoint
+            center = int(math.ceil(filterSize/2))
+            # Size on the left and right. Used for location. -1 b/c coordinates start at 0
+            filterSizeL = center-1
+            filterSizeR = filterSize-filterSizeL-1
+            # Lambda = wavelength
+            lmbda = (filterSize*2)/div[k]
             # Lower sigma = sharper edged filters, higher sigma = blurry edged filters
-            sigma = 0.0036 * i * i + 0.35 * i + 0.18
-            lmbda = sigma / 0.8
-            gamma = 0.3
+            sigma = (lmbda)*0.8
+            sigmaSquared = (sigma)**2
 
-            # Fill x2 and y2 (our template grid) with appropriate angles for this phase
-            x2 = x * np.cos(theta) + y * np.sin(theta)
-            y2 = -x * np.sin(theta) + y * np.cos(theta)
-            # Create Gaussian filter at the set orientation
-            filter = (np.exp(-(x2 * x2 + gamma * gamma * y2 * y2) / (2 * sigma * sigma)) * np.cos(2 * math.pi * x2 / lmbda))
+            f = np.zeros(((filterSizeR-(-filterSizeL)+center), (filterSizeR-(-filterSizeL)+center)))
 
-            filter[np.sqrt(x ** 2 + y ** 2) > (i / 2)] = 0.0
+            # Apply filter over the receptive field. Rfs are square, thus use same range()
+            for i in range(-filterSizeL, filterSizeR):
+                for j in range(-filterSizeL, filterSizeR):
+                    # If out of bounds, filter is zero
+                    if math.sqrt(i**2+j**2) > rfsizes[k]/2:
+                        e = 0
+                    else:
+                        # X coordinate for the filter
+                        x = i*math.cos(theta) - j*math.sin(theta)
+                        # Y coordinate for the filter
+                        y = i*math.sin(theta) + j*math.cos(theta)
+                        # Filter at that x,y coordinate
+                        e = math.exp(-((x**2)+(gamma**2)*(y**2))/(2*sigmaSquared))*math.cos(2*math.pi*x/lmbda)
+                    f[i+center][j+center] = e
             # Normalize
-            filter = filter - np.mean(filter)
-            filter = filter / np.sqrt(np.sum(filter ** 2))
-            filtersThisSize.append(filter.astype('float'))
+            f = f - np.mean(f)
+            f = f/np.sqrt(np.sum(f**2))
 
-        filters.append(filtersThisSize)
+            # Ith filter
+            iFilter =  numOrientations*(k-1) + o
+            filters[1:(filterSize**2)][iFilter] = np.reshape(f, (filterSize**2, 1))
+            filterSizes[iFilter] = filterSize
 
-    return filters
+
+
+
+
--- vision/preprocess.py
+++ vision/preprocess.py
@@ -2,6 +2,7 @@
 
 import numpy as np
 import cv2
+from vision.baseHierarchy import buildV1Filters
 
 filename = "/Users/Michael/Documents/Animatrix/testImages/lena.jpg"
 
@@ -14,5 +15,9 @@ print("Building V1 Filters")
 orientations = np.array([90, -45, 0, 45])
 # Initialize the different receptive field sizes
 rfsizes = np.arange(7, 39, 2)
+# Initialize scaling factors, tuning the wavelength of the sinusoidal factor 'lambda' in relation to each of the rfsizes
+div = np.arange(4, 3.2, -0.05)
+buildV1Filters.buildV1filters(orientations, rfsizes, div)
+
 
 
