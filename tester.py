e066caff931a4e7b18ffd00149c50a1309fb7644
mdelsole <mdelsole64@gmail.com>
2019-04-29 14:06:46 -0400

    Continued Work on V1

--- tester.py
+++ tester.py
@@ -2,6 +2,4 @@ import numpy as np
 
 # Defining 8 scale bands
 c1Scale = np.array([2, 2, 3, 0, 1, 2, 3, 4, 5])
-print(c1Scale)
-c1Scale[np.where(c1Scale == 0)] = 1
-print(c1Scale)
+print(c1Scale[:4])
--- vision/baseHierarchy/architecture/v1SimpleCells.py
+++ vision/baseHierarchy/architecture/v1SimpleCells.py
@@ -2,7 +2,10 @@ import math
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
+
 # View whole array, for debugging purposes
+from matplotlib import gridspec
+
 np.set_printoptions(threshold=np.nan)
 
 """
@@ -15,15 +18,23 @@ orientations (4 orientations per scale) while the outer loop runs through scales
 def v1SimpleCells(orientations, rfsizes, div):
 
     numOrientations = np.size(orientations)
+    print("numOrientations: ", numOrientations)
     numRfsizes = np.size(rfsizes)
-    numFilters = numRfsizes*numOrientations
+    print("numRFsizes: ", numRfsizes)
+    #Times 2 because 2 phases
+    numFilters = numRfsizes*numOrientations*2
+    print("numFilters: ", numFilters)
+
+    spec = gridspec.GridSpec(nrows=5, ncols=39)
+    fig = plt.figure()
 
     # Arrays: [how many down][how many across]
 
-    # Spatial aspect ratio
+    # Spatial aspect ratio. Controls the height of the function
     gamma = 0.3
     # Array of the filter sizes (17 different sets of 4 orientations, 1 for each rfsize)
     filterSizes = np.zeros((numFilters,1))
+    print(filterSizes.shape)
     # Storage place for the finalized filters
     # Column = biggest rfsize, squared because rfs are square. Row = for each different rfsize
     filters = np.zeros((np.max(rfsizes)**2,numFilters))
@@ -36,33 +47,39 @@ def v1SimpleCells(orientations, rfsizes, div):
             filterSize = rfsizes[k]
             # Center of filter, i.e. midpoint
             center = int(math.ceil(filterSize/2))
+            #print("Center: ", center)
             # Size on the left and right. Used for location. -1 b/c coordinates start at 0
-            filterSizeL = center
-            filterSizeR = filterSize-filterSizeL
-            # Lambda = wavelength
+            filterSizeL = center-1
+            #print("filterSizeL: ", filterSizeL)
+            filterSizeR = filterSize-filterSizeL-1
+            #print("filterSizeR: ", filterSizeL)
+            # Lambda = wavelength. Larger wavelengeth = thicker stripes. We make lambda proportional to the filter size
+            # because we want thicker stripes when we increase the size of the filter
             lmbda = (filterSize*2)/div[k]
+            #print("Lambda: ", lmbda)
             # Lower sigma = sharper edged filters, higher sigma = blurry edged filters
             sigma = (lmbda)*0.8
             sigmaSquared = (sigma)**2
 
-            f = np.zeros(((filterSizeR-(-filterSizeL)), (filterSizeR-(-filterSizeL))))
+            f = np.zeros(((filterSizeR-(-filterSizeL)+1), (filterSizeR-(-filterSizeL)+1)))
             #print("F Shape: ", f.shape)
 
-            # Apply filter over the receptive field. Rfs are square, thus use same range().
-            for i in range(-filterSizeL, filterSizeR):
-                for j in range(-filterSizeL, filterSizeR):
+            # Form the filter (draw it) over the receptive field. Rfs are square, thus use same range().
+            for i in range(-filterSizeL, filterSizeR+1):
+                for j in range(-filterSizeL, filterSizeR+1):
                     # If out of bounds, filter is zero
                     if math.sqrt(i**2+j**2) > filterSize/2:
+                        #print("Got Here, I, j, Fs/2: ", math.sqrt(i**2+j**2), filterSize/2)
                         e = 0
                     else:
                         # X coordinate for the filter
                         x = i*math.cos(theta) - j*math.sin(theta)
                         # Y coordinate for the filter
                         y = i*math.sin(theta) + j*math.cos(theta)
-                        # Filter at that x,y coordinate
+                        # Filter at that x,y coordinate. This is the equation for a gabor filter
                         e = math.exp(-((x**2)+((gamma**2)*(y**2)))/(2*sigmaSquared))*math.cos(2*math.pi*x/lmbda)
                     #print("J, I: ", j+center, i+center)
-                    f[j+center][i+center] = e
+                    f[j+center-1,i+center-1] = e
             # Normalize
             f = f - np.mean(f)
             f = f/np.sqrt(np.sum(f**2))
@@ -70,18 +87,24 @@ def v1SimpleCells(orientations, rfsizes, div):
 
             # Ith filter
             iFilter = numOrientations*(k) + o
+            #print("FilterSize: ", filterSize)
+            #print(filterSizes)
+            #F was a 1D
             filters[0:filterSize**2, iFilter] = np.reshape(f, (filterSize**2))
             filterSizes[iFilter] = filterSize
 
-    np.set_printoptions(precision=4)
-    numCells = filters.shape[0]
-    print(numCells)
-    SIDE = np.ceil(np.sqrt(numCells)).astype(int)
-    RFSIZE = int(np.sqrt(filters.shape[1]))
+            #Visualize it
+            ax = fig.add_subplot(spec[o,k])
+            ax.matshow(np.reshape(filters[:(filterSize**2), iFilter], (filterSize, filterSize)), cmap='Greys_r')
+            ax.set_axis_off()
 
-    ax = plt.subplot(SIDE, SIDE, 505 + 1)
-    ax.matshow(np.reshape(filters[505, :], (RFSIZE, RFSIZE)), cmap='Greys_r')
-    ax.set_axis_off()
+    #print(filters[:,0])
+    print("Filters: ", filters.shape)
+    print("FilterSizes: ", filterSizes.shape)
+    numCells = filters.shape[1]
+    print("NumFilters: ", numCells)
+    RFSIZE = int(np.sqrt(filters.shape[0]))
+    print(RFSIZE)
 
     plt.draw()
     #imgplot = plt.imshow(filterSizes, cmap=plt.get_cmap('gray'))
