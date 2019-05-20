0b3ed04bb9f2f2389628d7042b0ded1a77c7808f
mdelsole <mdelsole64@gmail.com>
2019-05-20 12:11:14 -0400

    Continued work on complex cells

--- tester.py
+++ tester.py
@@ -1,4 +1,7 @@
 import numpy as np
+import scipy.io as sio
+np.set_printoptions(threshold=np.nan)
 
 # Defining 8 scale bands
-print(np.ones((2*3+1,2*3+1)))
+mat_contents = sio.loadmat('/Users/Michael/Documents/Animatrix/universal_patch_set.mat')
+print(mat_contents)
